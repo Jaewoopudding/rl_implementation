fd9e760cd8949bc5c4dcc06f478408c74bc2c79b
Jaewoopudding <ijnn258@gmail.com>
2023-05-22 18:57:51 +0900

    DDPG Implementation, DQN modify

--- dqn.py
+++ dqn.py
@@ -12,26 +12,14 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 
+from utils import ReplayMemory, Transition
+
+
 is_ipython = 'inline' in matplotlib.get_backend()
 if is_ipython:
     from IPython import display
 
 plt.ion()
-
-Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
-
-class ReplayMemory(object):
-    def __init__(self, capacity):
-        self.memory = deque([], maxlen=capacity)
-        
-    def push(self, *args):
-        self.memory.append(Transition(*args))
-    
-    def sample(self, batch_size):
-        return random.sample(self.memory, batch_size)
-    
-    def __len__(self):
-        return len(self.memory)
     
 class DQN(nn.Module):
     def __init__(self, n_observations, n_actions, n_hidden=128):
